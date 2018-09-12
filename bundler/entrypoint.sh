dc0bc1e1d7fcfff07a57febc65c1608e5275aaaf
Nikos Maris <nmaris@crypteianetworks.com>
2018-09-13 00:00:34 +0300

    keep one perf.sh for all top level folders

--- bundler/entrypoint.sh
+++ bundler/entrypoint.sh
@@ -8,5 +8,5 @@ run(){
 run bundle install
 
 echo "stopping the container as the goal to measure build and run times and not to build and run a service..."
-#rackup --host 0.0.0.0
+#rackup --host 0.0.0.0 -p 8080
 
--- perf.sh
+++ perf.sh
@@ -1,16 +1,18 @@
 #!/bin/sh +ex
 
+cd $1
+
 docker build --pull --no-cache -t my-image .
 docker history my-image
 echo "timestamp: starting my-container"
-docker run -v `pwd`:/src -p 8888:9292 --name my-container my-image
+docker run -v `pwd`:/src -p 8080:8080 --name my-container my-image
 echo "timestamp: stop and start my-container"
 # docker stop my-container
 docker start -a my-container
 echo "timestamp: stop, rm and run my-container"
 # docker stop my-container
 docker rm my-container
-docker run -v `pwd`:/src -p 8888:9292 --name my-container my-image
+docker run -v `pwd`:/src -p 8080:8080 --name my-container my-image
 
 # cleanup for next run if any
 # docker stop my-container
