58f3dade44c23f421f4cad6c93dd70588a8387ac
Nikos Maris <nmaris@crypteianetworks.com>
2018-09-13 00:17:22 +0300

    remove ports as the demos are not webapps

--- perf.sh
+++ perf.sh
@@ -5,14 +5,14 @@ cd $1
 docker build --pull --no-cache -t my-image .
 docker history my-image
 echo "timestamp: starting my-container"
-docker run -v `pwd`:/src -p 8080:8080 --name my-container my-image
+docker run -v `pwd`:/src --name my-container my-image
 echo "timestamp: stop and start my-container"
 # docker stop my-container
 docker start -a my-container
 echo "timestamp: stop, rm and run my-container"
 # docker stop my-container
 docker rm my-container
-docker run -v `pwd`:/src -p 8080:8080 --name my-container my-image
+docker run -v `pwd`:/src --name my-container my-image
 
 # cleanup for next run if any
 # docker stop my-container
